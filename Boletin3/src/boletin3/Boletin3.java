04a399a964555ffaf6cf243ecd11096f18e0923a
PabloKrespo <56074061+pablokrespo@users.noreply.github.com>
2019-10-09 14:16:05 +0200

    Update Boletin3.java

--- Boletin3/src/boletin3/Boletin3.java
+++ Boletin3/src/boletin3/Boletin3.java
@@ -15,7 +15,9 @@ public class Boletin3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // 
+        ESPARTANOOOOOOOS
+            CUAL ES VUESTRO OFICIO??
     }
     
 }
